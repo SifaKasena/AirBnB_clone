46520d9438c786490949334b15765056027f0e81
SifaKasena <sifa.collins@gmail.com>
2023-05-23 11:42:22 +0300

    Refactor code

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This module contains the base class"""
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
@@ -21,7 +21,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self) -> str:
         """Returns a string representation of a BaseModel instance"""
@@ -30,7 +30,7 @@ class BaseModel:
     def save(self) -> None:
         """Updates the updated_at attribute of an instance"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self) -> dict:
         """Returns a dictionary representation of the instance"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,13 @@ This module contains the class and method that handle
 serialization and desrilization of python objects
 """
 import json
-
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class FileStorage:
     """Contains methods for serilization and desirilization"""
@@ -34,27 +40,11 @@ class FileStorage:
         (only if the JSON file (__file_path) exists; otherwise, do nothing.
         If the file doesn't exist, no exception should be raised)
         """
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-        class_names = {
-            'BaseModel': BaseModel,
-            'User': User,
-            'State': State,
-            'City': City,
-            'Amenity': Amenity,
-            'Place': Place,
-            'Review': Review
-        }
         try:
             with open(self.__file_path, "r", encoding="utf-8") as f:
                 json_dict = {}
                 for k, v in json.load(f).items():
-                    json_dict[k] = class_names[v['__class__']](**v)
+                    json_dict[k] = eval(v['__class__'])(**v)
                 self.__objects = json_dict
         except FileNotFoundError:
             pass
